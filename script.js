14784e674da329a69db9f795cc9259e2c120a415
elza-2001 <elzik998@gmail.com>
2025-02-28 19:21:46 +0500

    домашняя работа 10

--- script.js
+++ script.js
@@ -1,8 +1,42 @@
 const form = document.querySelector(".form");
+const nameInput = document.querySelector("#name");
+const secondNameInput = document.querySelector("#secondName");
+const emailInput = document.querySelector("#email");
+const phoneInput = document.querySelector("#phone");
+const checkboxInput = document.querySelector("#agree");
+
 form.addEventListener("submit", (event) => {
-  // Предотвращает действие браузера по умолчанию. В данном случае — отправку формы
-  // https://learn.javascript.ru/default-browser-action
   event.preventDefault();
 
-  // Здесь твой код
+  let name = nameInput.value;
+  let secondName = secondNameInput.value;
+  let email = emailInput.value;
+  let phone = phoneInput.value;
+  let checkbox = checkboxInput.checked;
+
+  fetch('https://polinashneider.space/user', {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer: elza-2001'
+    },
+    body: JSON.stringify({
+      "name": name,
+      "secondName": secondName,
+      "phone": phone,
+      "email": email,
+      "agree": checkbox
+    }),
+  })
+  .then((result) => {
+    return result.json()
+  })
+  .then((data) => {
+    form.reset();
+    alert('Данные успешно отправлены');
+  })
+  .catch((error) => {
+    alert('Произошла ошибка при отправке данных');
+  })
 });
